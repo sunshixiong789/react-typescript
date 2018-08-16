e071969cc4590e16652243173e1bb271bbbcbb1f
sunshixiong <1285913468@qq.com>
2018-08-16 14:01:34 +0800

    添加项目需要的包

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@material-ui/core";
+import ArrowIeft from "@material-ui/icons/ArrowBack";
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Grid container={true}>
+              <Grid item={true} xs={3}>
+                <IconButton color="inherit">
+                  <ArrowIeft />
+                </IconButton>
+              </Grid>
+              <Grid
+                  container={true}
+                  item={true}
+                  xs={6}
+                  alignItems="center"
+                  justify={"center"}
+              >
+                <Typography variant="title" color="inherit">
+                  { "没有标题"}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
